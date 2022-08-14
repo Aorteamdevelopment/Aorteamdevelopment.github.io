2c3b16e1971000ecddf8bf4521ba8511944a5f74
Aorteamdevelopment <109992456+Aorteamdevelopment@users.noreply.github.com>
2022-08-14 17:24:18 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+alert('Hai ka mei');
+alert('aku punya pantun buat ka mei');
+alert('abcdefghiloveyou');
+alert('ka mei mau ga jadi pacar akuu');
+alert('aku maksa kak:)');
+alert('mau yaa');
+alert('yaa');
+alert('pliss kak:)');
